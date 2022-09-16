f34056ece7c470ee2e362dadb2289614a69ef808
Bernard Riemann <riemann.br@gmail.com>
2022-09-17 00:49:39 +0200

    playing around with a scanner

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,50 @@
 use pyo3::prelude::*;
+use std::io::prelude::*;
 
 use std::fs::File;
 use std::os::unix::prelude::FileExt;
+use std::io::BufReader;
 use std::str;
 
 #[pyclass]
 struct FunPyre {
     file : File,
+}
+
+fn scan(segment: Vec<u8>) -> u8 {
+    let mut n = 0;
+    for be in segment {
+        if be > 60 {
+            n += 1
+        }
+    }
+    n
+}
+
+impl FunPyre {
+    fn rfrom_index(&self, start_index: u64, n_bytes: usize) -> Result<String, &str> {
+        let mut buf = vec![0; n_bytes];
+        self.file.read_exact_at(&mut buf, start_index).unwrap();
+        match str::from_utf8(&buf) {
+            Ok(ostr) => Ok(ostr.to_string()),
+            Err(_) => Err("utf-8 problem")
+        }
+    }
+
+    fn bscan(&mut self, iterations: usize) -> Result<u8, &str> {
+        let buf_reader = BufReader::new(&mut self.file);
+        let mut x;
+        for (n, segmentread) in buf_reader.split(b' ').enumerate() {
+            match segmentread {
+                Ok(segment) => x = scan(segment),
+                Err(_) => return Err("bla")
+            }
+            if n>iterations {
+                return Ok(x)
+            }
+        }
+        Ok(42)
+    }
 }
 
 #[pymethods]
@@ -20,11 +58,18 @@ impl FunPyre {
     }
 
     fn from_index(self_: PyRef<'_, Self>, start_index: u64, n_bytes: usize) -> PyResult<String> {
-        let mut buf = vec![0; n_bytes];
-        self_.file.read_exact_at(&mut buf, start_index)?;
-        let ostr = str::from_utf8(&buf)?;
-        return Ok(ostr.to_string())
+        Ok(self_.rfrom_index(start_index, n_bytes).unwrap())
     }
+
+    fn scan(mut self_: PyRefMut<'_, Self>, iterations: usize) -> PyResult<[u8;2]> {
+        let x = self_.bscan(iterations).unwrap();
+
+        // plan is to also return flags if is english sentence etc.
+        let mut arr = [0;2];
+        arr[1] = x;
+        return Ok(arr)
+    }
+
 }
 
 #[pymodule]
