5038c22c234077d1c09642d54258f7fcac4f8346
Bernard Riemann <riemann.br@gmail.com>
2022-09-28 22:22:12 +0200

    works but page-internal scanner is super-slow, dont know why yet

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,10 @@ use std::io::BufReader;
 use std::io::SeekFrom;
 use std::str;
 
-use regex::bytes::Regex;
+//use regex::bytes::Regex;
 
 #[pyclass]
+#[derive(Clone)]
 struct Segment {
     #[pyo3(get)]
     start: u64,
@@ -24,21 +25,62 @@ impl Segment {
     }
 }
 
-fn is_sentence(segment: &Vec<u8>) -> bool {
-    //single-character based classification of segments.
-    // i.e. checks if standard english sentence
-    for be in segment {
-        match be {
-          0x41..=0x5a => continue, //uppercase
-          0x61..=0x7a => continue, //lowercase
-          b'.' => continue,
-          b',' => continue,
-          b' ' => continue,
-          _ => return false
+//fn is_sentence(segment: &Vec<u8>) -> bool {
+//    //single-character based classification of segments.
+//    // i.e. checks if standard english sentence
+//    for be in segment {
+//        match be {
+//          0x41..=0x5a => continue, //uppercase
+//          0x61..=0x7a => continue, //lowercase
+//          b'.' => continue,
+//          b',' => continue,
+//          b' ' => continue,
+//          _ => return false
+//        }
+//    }
+//    return true
+//}
+
+fn asplit_buf<T>(buf: &mut T, split_at: &Vec<u8>, split_left: bool, iterations: usize) -> Vec<u64>
+    where T: BufRead + Seek {
+    let mut out = Vec::with_capacity(iterations);
+    out.push( buf.stream_position().unwrap() );
+
+    let mut tmp = Vec::with_capacity(512);
+    let split = split_at.last().unwrap().clone();
+    let splen = split_at.len();
+
+    'iters: loop {
+        let readlen = buf.read_until(split, &mut tmp).unwrap();
+        if readlen == 0 {
+            return out
         }
+        if readlen < splen {
+            continue
+        }
+
+        let trunclen = tmp.len() - splen;
+        for m in 0..splen-1 {
+            if tmp[trunclen+m] != split_at[m] {
+                continue 'iters
+            }
+        }
+
+        // if split_left {
+        //     tmp.truncate(trunclen); //tmp is not used at the moment
+        // }
+
+        out.push( buf.stream_position().unwrap() - if split_left { splen as u64 } else { 0 } );
+        if out.len() == iterations {
+            return out
+        }
+
+        //if split_left {
+        //    tmp = split_at.clone();
+        tmp.clear();
     }
-    return true
 }
+
 
 #[pyclass]
 struct Segments {
@@ -55,17 +97,12 @@ impl Segments {
     }
 }
 
-fn is_page(segment: &Vec<u8>) -> bool {
-    let re = Regex::new(r"^<page>\n    <title>.*</title>\n    <id>(\d*)</id>\n    [\s\S]*<revision>\n      <id>(\d*)</id>\n      <timestamp>20(\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d)Z</timestamp>\n      <contributor>\n        [\s\S]+\n      </contributor>\n[\s\S]+$").unwrap();
-    re.is_match(segment)
-}
-
-
 #[pyclass]
 struct FunPyre {
     file: File,
-    //pages: Segments
 }
+
+
 
 impl FunPyre {
     fn rfrom_index(&self, start_index: u64, n_bytes: usize) -> Result<String, &str> {
@@ -82,49 +119,12 @@ impl FunPyre {
         self.rfrom_index(seg.start, n_bytes)
     }
 
-    fn ascan(&self, split_at: &Vec<u8>, split_left: bool, iterations: usize) -> Result<Segments, &str>
+    fn ascan(&self, split_at: &Vec<u8>, split_left: bool, iterations: usize, start: u64) -> Result<Segments, &str>
     {
         let mut buf_reader = BufReader::new(&self.file);
-        buf_reader.seek(SeekFrom::Start(0)).unwrap();
-        
-        let mut tmp = Vec::with_capacity(512);
-        let mut out = Vec::with_capacity(iterations);
-        out.push(0);
-
-        let split = split_at.last().unwrap().clone();
-        let splen = split_at.len();
-
-        'iters: loop {
-            let readlen = buf_reader.read_until(split, &mut tmp).unwrap();
-            if readlen == 0 {
-                return Ok(Segments{ indices: out })
-            }
-            if readlen < splen {
-                continue
-            }
-
-            let trunclen = tmp.len() - splen;
-            for m in 0..splen-1 {
-                if tmp[trunclen+m] != split_at[m] {
-                    continue 'iters
-                }
-            }
-
-            // if split_left {
-            //     tmp.truncate(trunclen); //tmp is not used at the moment
-            // }
-
-            out.push( buf_reader.stream_position().unwrap() - if split_left { splen as u64 } else { 0 } );
-            if out.len() == iterations {
-                return Ok(Segments{ indices: out })
-            }
-
-            //if split_left {
-            //    tmp = split_at.clone();
-            //} else {
-                tmp.clear();
-            //}
-        }
+        buf_reader.seek(SeekFrom::Start(start)).unwrap();
+        let indices = asplit_buf(&mut buf_reader, split_at, split_left, iterations);
+        Ok( Segments{ indices } )
     }
 }
 
@@ -144,17 +144,50 @@ impl FunPyre {
     }
 
     fn find_sentences(self_: PyRef<'_, Self>, iterations: usize) -> PyResult<Segments> {
-        Ok( self_.ascan(&b".".to_vec(), false, iterations).unwrap() )
+        Ok( self_.ascan(&b".".to_vec(), false, iterations, 0).unwrap() )
     }
 
     fn find_pages(self_: PyRef<'_, Self>, iterations: usize) -> PyResult<Segments> {
-        Ok( self_.ascan(&b"<page".to_vec(), true, iterations).unwrap() )
+        Ok( self_.ascan(&b"<page".to_vec(), true, iterations, 0).unwrap() )
     }
 
 }
 
+
+#[pyclass]
+struct Page {
+    #[pyo3(get)]
+    head: Segment,
+    #[pyo3(get)]
+    content: Segment
+}
+
+#[pymethods]
+impl Page {
+    #[new]
+    fn new(seg: &Segment, f: &FunPyre) -> Page {
+        let head = f.ascan(&b"</contributor>\n".to_vec(), false, 1, seg.start).unwrap().getitem(0);
+        let content = Segment{ start: head.end, end: seg.end };
+        Page { head, content }
+    }
+
+    //{
+    //    let re = Regex::new(r"^<page>\n    <title>.*</title>\n    <id>(\d*)</id>\n    [\s\S]*<revision>\n      <id>(\d*)</id>\n      <timestamp>20(\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d)Z</timestamp>\n      <contributor>\n        [\s\S]+\n      </contributor>\n[\s\S]+$").unwrap();
+    //    let caps = re.captures(seg).unwrap();
+    //    Page {
+    //        title: caps[1],
+    //        id: caps[3],
+    //        rev_id: caps[4]
+    //        rev_timestamp: 
+    //    }
+
+    //}
+}
+
+
 #[pymodule]
 fn pyre(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<FunPyre>()?;
+    m.add_class::<Page>()?;
     Ok(())
 }
