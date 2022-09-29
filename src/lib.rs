dd246d3c743e9f4639460231d843d1075e66dd9f
Bernard Riemann <riemann.br@gmail.com>
2022-09-29 04:15:26 +0200

    tokenizing, and simple example in main

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ use regex::bytes::Regex;
 
 #[pyclass]
 #[derive(Clone)]
-struct Segment {
+pub struct Segment {
     #[pyo3(get)]
     start: u64,
     #[pyo3(get)]
@@ -84,13 +84,13 @@ fn asplit_buf<T>(buf: &mut T, split_at: &Vec<u8>, split_left: bool, mut iteratio
 
 
 #[pyclass]
-struct Segments {
+pub struct Segments {
    indices: Vec<u64>
 }
 
 #[pymethods]
 impl Segments {
-    fn getitem(&self, n: usize) -> Segment {
+    pub fn getitem(&self, n: usize) -> Segment {
         Segment {
             start: self.indices[n],
             end: self.indices[n+1]
@@ -99,7 +99,7 @@ impl Segments {
 }
 
 #[pyclass]
-struct FunPyre {
+pub struct FunPyre {
     file: File,
 }
 
@@ -112,11 +112,6 @@ impl FunPyre {
         buf
     }
 
-    fn rfrom_segment(&self, seg: &Segment) -> Vec<u8> {
-        let n_bytes = (seg.end - seg.start) as usize;
-        self.rfrom_index(seg.start, n_bytes)
-    }
-
     fn ascan(&self, split_at: &Vec<u8>, split_left: bool, iterations: usize, start: u64) -> Result<Segments, &str>
     {
         let mut buf_reader = BufReader::new(&self.file);
@@ -126,10 +121,30 @@ impl FunPyre {
     }
 }
 
+fn uvec_to_string(uvec: &Vec<u8>) -> PyResult<String> {
+    //Ok( str::from_utf8_unchecked(&uvec) )
+    match str::from_utf8(&uvec) {
+        Ok(ostr) => Ok(ostr.to_string()),
+        Err(_) => Err(PyTypeError::new_err("utf-8 problem"))
+    }
+}
+
+impl FunPyre {
+    pub fn find_pages(&self, iterations: usize) -> Result<Segments, &str> {
+        self.ascan(&b"<page".to_vec(), true, iterations, 0)
+    }
+
+    pub fn rfrom_segment(&self, seg: &Segment) -> Vec<u8> {
+        let n_bytes = (seg.end - seg.start) as usize;
+        self.rfrom_index(seg.start, n_bytes)
+    }
+}
+
+
 #[pymethods]
 impl FunPyre {
     #[new]
-    fn new(filename: String) -> Self {
+    pub fn new(filename: String) -> Self {
         let file = File::open(filename).unwrap();
         FunPyre {
             file,
@@ -138,32 +153,18 @@ impl FunPyre {
     }
 
     fn from_segment(self_: PyRef<'_, Self>, segment: &Segment) -> PyResult<String> {
-        let buf = self_.rfrom_segment(segment);
-        match str::from_utf8(&buf) {
-            Ok(ostr) => Ok(ostr.to_string()),
-            Err(_) => Err(PyTypeError::new_err("utf-8 problem"))
-        }
+        uvec_to_string( &self_.rfrom_segment(segment) )
     }
 
     fn find_sentences(self_: PyRef<'_, Self>, iterations: usize) -> PyResult<Segments> {
         Ok( self_.ascan(&b".".to_vec(), false, iterations, 0).unwrap() )
     }
 
-    fn find_pages(self_: PyRef<'_, Self>, iterations: usize) -> PyResult<Segments> {
-        Ok( self_.ascan(&b"<page".to_vec(), true, iterations, 0).unwrap() )
+    fn ffind_pages(self_: PyRef<'_, Self>, iterations: usize) -> PyResult<Segments> {
+        Ok( self_.find_pages(iterations).unwrap() )
     }
 
 }
-
-enum Token {
-    AsciiWord{ wrd: Vec<u8> },
-    PageMacro {
-        id: u8,
-        rev_id: u32,
-        tstamp_2k: u32,
-    }
-} 
-
 
 #[pyclass]
 struct Page {
@@ -173,8 +174,37 @@ struct Page {
     content: Segment
 }
 
-const PAGEBYTE : u8 = b'\x00';
+// these bytes should not be part of the input file (they aren't for enwik9),
+// and preferably still be part of the ascii table (for easier utf8 output for conversion if required
+const MACROBYTE : u8 = b'\x05'; // x05 is nice, as its the enquiry symbol in ascii
+const ANTISPACE : u8 = b'\x15'; // x08 would be nice, as its backspace in ascii
 
+pub fn tokenize_page(invec: &Vec<u8>) -> Vec<u8> {
+    let re = Regex::new(r"^<page>\n    <title>(.*)</title>\n    <id>(\d*)</id>\n    [\s\S]*<revision>\n      <id>(\d*)</id>\n      <timestamp>20(\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d)Z</timestamp>\n      <contributor>\n        [\s\S]+\n      </contributor>\n([\s\S]+)$").unwrap();
+    let caps = re.captures( &invec ).unwrap();
+    let mut ot = vec![MACROBYTE, b'p', b'a', b'g', b'e', b' '];
+    ot.extend_from_slice( &caps[1] );
+    ot.push(b' ');
+    ot.extend_from_slice( &caps[2] );
+    ot.push(b' ');
+    ot.extend_from_slice( &caps[4] );
+    ot.push(b' ');
+    ot.extend_from_slice( &caps[5] );
+    ot.extend_from_slice( &caps[6] );
+    ot.extend_from_slice( &caps[7] );
+    ot.extend_from_slice( &caps[8] );
+    ot.extend_from_slice( &caps[9] );
+    ot.extend(b" \n ");
+    //main text
+    ot.extend_from_slice( &caps[10] );
+    ot.push(ANTISPACE); //just for testing
+    ot
+}
+
+#[pyfunction]
+fn tokenize_page_string(seg: &Segment, f: &FunPyre) -> PyResult<String> {
+    uvec_to_string( &tokenize_page( &f.rfrom_segment(seg) ) )
+}
 
 #[pymethods]
 impl Page {
@@ -184,21 +214,6 @@ impl Page {
         let content = Segment{ start: head.end, end: seg.end };
         Page { head, content }
     }
-
-    fn tokenize_head(&self, f: &FunPyre) -> Vec<u8> { // ToDo: Buffer instead of Vec
-        let re = Regex::new(r"^<page>\n    <title>.*</title>\n    <id>(\d*)</id>\n    [\s\S]*<revision>\n      <id>(\d*)</id>\n      <timestamp>20(\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d)Z</timestamp>\n      <contributor>\n        [\s\S]+\n      </contributor>\n[\s\S]+$").unwrap();
-
-        let input = f.rfrom_segment(&self.head);
-        //let caps = re.captures(  ).unwrap();
-
-        let mut out = Vec::with_capacity(256);
-        //let id = caps[2];
-        //let rev_id = caps[3];
-        //let tstamp_2k : u32 = caps[9] +60*(caps[8] + 60*(caps[7] + 24*(caps[6] + 31*(caps[5] + 12*caps[4]))));
-
-        out.push(PAGEBYTE);
-        out
-    }
 }
 
 
@@ -206,5 +221,6 @@ impl Page {
 fn pyre(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<FunPyre>()?;
     m.add_class::<Page>()?;
+    m.add_wrapped(wrap_pyfunction!(tokenize_page_string)).unwrap();
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod lib;
+use lib::FunPyre;
+use lib::tokenize_page;
+use std::str;
+
 fn main() {
     println!("Hello, world!");
+    let fp = FunPyre::new("../workfiles/enwik9".to_string());
+    let segments = fp.find_pages(300);
+
+    let iv = fp.rfrom_segment( &segments.unwrap().getitem(7) );
+    //print!("--ORIGINAL--\n{}\n", str::from_utf8(&iv).unwrap());
+
+    let ev = tokenize_page(&iv);
+    print!("--TOKENIZED--\n{}\n", str::from_utf8(&ev).unwrap());
 }
